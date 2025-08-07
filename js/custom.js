d4dd2c738aefe13457753589c9eadea5b7a3c315
Arpan Datta <arpan.dutta@esolzmail.com>
2025-08-07 21:55:57 +0530

    react update

--- js/custom.js
+++ js/custom.js
@@ -325,17 +325,17 @@ $(document).ready(function () {
 
 // Disabled inspect mode 
 
-// document.addEventListener('contextmenu', event => event.preventDefault());
-// document.onkeydown = function (e) {
-//   if (
-//     e.keyCode === 123 || // F12
-//     (e.ctrlKey && e.shiftKey && e.keyCode === 73) || // Ctrl+Shift+I
-//     (e.ctrlKey && e.shiftKey && e.keyCode === 74) || // Ctrl+Shift+J
-//     (e.ctrlKey && e.keyCode === 85) // Ctrl+U (View source)
-//   ) {
-//     return false;
-//   } 
-// };
+document.addEventListener('contextmenu', event => event.preventDefault());
+document.onkeydown = function (e) {
+  if (
+    e.keyCode === 123 || // F12
+    (e.ctrlKey && e.shiftKey && e.keyCode === 73) || // Ctrl+Shift+I
+    (e.ctrlKey && e.shiftKey && e.keyCode === 74) || // Ctrl+Shift+J
+    (e.ctrlKey && e.keyCode === 85) // Ctrl+U (View source)
+  ) {
+    return false;
+  } 
+};
 
 // Disabled inspect mode 
 
